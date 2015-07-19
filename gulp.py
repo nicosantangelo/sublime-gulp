0e71d80d41f41c4a866f8d92a54821074f8c01fa
Nicolás Santángelo <santangelonicolas@gmail.com>
2015-07-19 14:20:27 -0300

    Remove logging

--- gulp.py
+++ gulp.py
@@ -160,8 +160,6 @@ class GulpCommand(BaseCommand):
         Thread(target = self.run_process, args = (task, )).start() # Option to kill on timeout?
 
     def construct_gulp_task(self):
-        print("HEREEEEEEEEEEEEEEE")
-        print(self.task_flag)
         self.show_running_status_in_output_panel()
         return r"gulp %s %s" % (self.task_name, self.task_flag)
 
