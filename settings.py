6231c3c8181e1785f7ff3d297c18d842f03ff755
Nicolás Santángelo <santangelonicolas@gmail.com>
2015-09-29 00:13:47 -0300

    Chores

--- settings.py
+++ settings.py
@@ -3,16 +3,17 @@ import sublime
 class Settings():
     def __init__(self):
         active_view = sublime.active_window().active_view()
-        self._sources = [active_view.settings(), ProjectData(), sublime.load_settings("Gulp.sublime-settings")]
+        self.user_settings = sublime.load_settings("Gulp.sublime-settings")
+        self.sources = [active_view.settings(), ProjectData(), self.user_settings]
 
     def get(self, key, default=None):
-        return next((settings.get(key, default) for settings in self._sources if settings.has(key)), None)
+        return next((settings.get(key, default) for settings in self.sources if settings.has(key)), None)
 
     def get_from_user_settings(self, key, default=None):
-        return self._sources[-1].get(key, default)
+        return self.user_settings.get(key, default)
 
     def has(self, key):
-        return any(settings.has(key) for settings in self._sources)
+        return any(settings.has(key) for settings in self.sources)
 
 
 class ProjectData():
