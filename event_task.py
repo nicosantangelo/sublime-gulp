44d372e8e05eb5a83731a7033c60d054195677d7
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-03-27 00:45:02 -0300

    Add support for `silent_tasks_on_save` and SublimeText 2 support

--- event_task.py
+++ event_task.py
@@ -1,7 +1,7 @@
 import sublime
 import sublime_plugin
-import fnmatch
 import os
+from fnmatch import fnmatch
 
 is_sublime_text_3 = int(sublime.version()) >= 3000
 
@@ -11,20 +11,22 @@ else:
 	from settings import Settings
 
 class EventTask(sublime_plugin.EventListener):
-
 	def on_post_save(self, view):
-		task_on_save = Settings().get("tasks_on_save", {})
-		if task_on_save is not None:
-			for key in task_on_save:
-				value = task_on_save[key]
-				if isinstance(value, str):
-					self.run(view, key, value)
-				elif isinstance(value, list):
-					for pattern in value:
-						self.run(view, key, pattern)
+		self.view = view
+		self.run_tasks(Settings().get("tasks_on_save", {}))
+		self.run_tasks(Settings().get("silent_tasks_on_save", {}), silent=True)
 
-	def run(self, view, task, pattern):
-		folders = view.window().folders()
-		pattern = folders[0] + os.sep + pattern if folders else ""
-		if fnmatch.fnmatch(view.file_name(), pattern):
-			view.window().run_command("gulp", {"task_name": task})
+	def run_tasks(self, tasks_on_save, silent=False):
+		if tasks_on_save:
+			for key in tasks_on_save:
+				value = tasks_on_save[key]
+				if isinstance(value, list):
+					for pattern in value:
+						self.run(key, pattern, silent)
+				else:
+					self.run(key, value, silent)
+
+	def run(self, task, pattern, silent):
+		folders = self.view.window().folders() or []
+		if any(fnmatch(self.view.file_name(), os.path.join(folder, pattern)) for folder in folders):
+			self.view.window().run_command("gulp", { "task_name": task, "silent": silent })
